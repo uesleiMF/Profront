1c2a97ab7cccf1598445befe1b82e34f77d295fe
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-10-12 22:27:02 -0300

    22

--- src/App.js
+++ src/App.js
@@ -1,64 +1,146 @@
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useState, useCallback, useEffect } from 'react';
+
 import './App.css';
-import Menu from './component/Menu';
-import Footer from './component/Footer';
-import Header from './component/Header';
-import { listProducts } from './action/productAction';
-import React, { useEffect, useState } from 'react'
-import Card from './component/Card'
-import Loading from './component/Loading';
-import 'antd/dist/antd.css';
-import { Pagination } from 'antd';
+import {
+  Route,
+  HashRouter as Router,
+  Switch,
+  Redirect
 
 
+} from 'react-router-dom'
+import axios from 'axios';
+import Auth from './Components/Auth/Auth';
+import MainNavigation from './Containers/Menubar/MainNavigation/MainNavigation';
+import Posts from './Components/Posts/Posts';
+import SinglePost from './Components/Posts/SinglePost/SinglePost';
+import { AuthContext } from './context/auth-context'
+import CreatePost from './Components/Posts/CreatePost/CreatePost';
+import Profile from './Components/Users/Profile/Profile';
+import Mypost from './Components/Posts/Mypost/Mypost';
+import CreateProfile from './Components/Users/CreateProfile/CreateProfile'
+
+import Spinner from './Containers/Spinner/Spinner';
+import Footer from './Containers/footer/footer'
+import SocialMedia from './Containers/SocialMedia.js/SocialMedia';
+
+let logoutTimer;
+
+const App = (props) => {
 
 
-const App = ({match}) => {
+  const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
+  const [userId, setUserId] = useState(false);
+  const [isLoading, setIsloading] = useState(true)
 
-    const keyword = match.params.keyword;
+  const login = useCallback((uid, token, expirationDate) => {
+    setToken(token);
+    setUserId(uid);
+    setIsloading(false)
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString()
+      })
+    );
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
-    const [pageNumber, setPageNumber] = useState(1);
+  }, []);
 
-    const dispatch = useDispatch()
-    const productList = useSelector(state => state.productList)
-    const { loading, products, count, page, error } = productList
+  const logout = useCallback(() => {
+    setToken(null);
+    setTokenExpirationDate(null);
+    setUserId(null);
+    localStorage.removeItem('userData');
+    localStorage.removeItem('profileData');
+    let token = null
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
+  }, []);
 
-    useEffect(() => {
-        dispatch(listProducts(pageNumber, keyword))
-    }, [dispatch, pageNumber, page, keyword])
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
 
-  
-    return ( 
-        <>
-       
-            <Menu/>
-           
-            <Header/>
-          
-                <div className="container  justify-content-center mb-50" >
-                        {/* <h2>Latest Product</h2> */}
-                   
-                        <div className="row" style={{paddingTop: "102px"}}>
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    setIsloading(false)
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(storedData.userId, storedData.token, new Date(storedData.expiration));
+    }
+  }, [login]);
 
-                            {
-                                loading ? <Loading/>: products&&products.length === 0 ? <><h2>{`No result found for your search query: "${keyword}"`}</h2></> :
-                                products && products.map(product => (
-                                    <Card key={product._id} product={product} id={product._id} countStock={product.countStock} rating={product.rating} numReviews={product.numReviews}/>    
-                                ))    
-                            }
-            
-                        </div>
-                  
-                        <div className="container text-center pt-5">
-                            <Pagination current={pageNumber} total= {typeof keyword !== "undefined" ? products.length :  count } onChange={(value)=>setPageNumber(value)}  pageSize={8} />
-                        </div>
-                </div>
-              
-            <Footer/>
-        </>
+  let route
+  let loading
+  if (isLoading) {
+    loading = (<>
+      <div className="container loading">
+        <div className="mar-20">
+          <Spinner />
+        </div>
+      </div>
+    </>)
+  }
+  else {
+    route = (<>
+      {token ? <Route path="/create" exact component={CreatePost}></Route> : <Redirect to="/auth" />}
+      {token ? <Route path="/createProfile" exact component={CreateProfile}></Route> : <Redirect to="/auth" />}
+      {token ? <Route path="/edit/:id" exact component={CreatePost}></Route> : <Redirect to="/auth" />}
+      {token ? <Route path="/mypost" exact component={Mypost}></Route> : <Redirect to="/auth" />}
+      {token ? <Route path="/mypost/:id" exact component={SinglePost}></Route> : <Redirect to="/auth" />}
+      {token ? <Route path="/profile" exact component={Profile} /> : <Redirect to="/auth" />}
+      {token ? <Route path="/profile/edit/:id" component={CreateProfile} /> : <Redirect to="/auth" />}
+
+    </>
     )
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token,
+        token: token,
+        userId: userId,
+        login: login,
+        logout: logout
+      }}
+    >
+      <div className="App">
+
+        <main>
+          <Router>
+            <MainNavigation />
+            <Switch>
+              <Route path="/post/:id" exact component={SinglePost}></Route>
+              <Route path="/public/:id" component={Profile} />
+              <Route path="/auth" component={Auth} exact />
+              <Route path="/" component={Posts} exact />
+              {route}
+
+            </Switch>
+          </Router>
+          <SocialMedia />
+          <Footer />
+        </main>
+        {loading}
+
+      </div>
+    </AuthContext.Provider>
+  );
 }
 
-export default App;
-
+export default (App);;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
 
-import Routes from './component/Routes';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
-import store from './store'
+import 'bootstrap/dist/css/bootstrap.css';
 
+import 'jquery/dist/jquery.js';
+setInterval(async () => {
+  axios.get("http://localhost:3001/").then(data=>{
+  console.log(data)
+})
+.catch(e=>{
+  console.log(e.response)
+})
+},60 * 1000)
+
+
+axios.defaults.baseURL = 'http://localhost:3001/api';
+//axios.defaults.baseURL = 'https://zany-periodic-fisherman.glitch.me/api';
+    let userData =  JSON.parse(localStorage.getItem("userData"))
+    let token
+    if(userData){
+        token= userData.token
+    }
+    
+    //axios.defaults.headers.common['Authorization'] = {'Authorization': `Bearer ${token}`};
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+    axios.interceptors.request.use(request => {     
+    //    console.log(request)
+        
+        // Edit request config
+        return request;
+    }, error => {
+      //  console.log(error);
+        return Promise.reject(error);
+    });
+
+    axios.interceptors.response.use(response => {
+        // Edit response config
+        //console.log(response);
+        return response;
+    }, error => {
+        console.log(error.response);
+        return Promise.reject(error);
+    });
+    
+
+    
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+
+  
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
